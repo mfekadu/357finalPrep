210bf085d619eff31a4e52f21412739d945b56b8
Michael Fekadu <mike.fekadu@gmail.com>
2018-03-19 10:53:59 -0700

    fixed to work on curr working dir

--- is_leaf_directory.c
+++ is_leaf_directory.c
@@ -7,44 +7,81 @@
 #include <limits.h>
 #include <sys/stat.h>
 #include <dirent.h>
+#include <string.h>
+
+#define LSTAT_ERROR "why the fudge do you want to check if the" \
+                     " PREVIOUS DIRECTORY is a LEAF?!?" \
+                     " lstat only works with the current working dir."
 
 int is_leaf_directory(const char *path) {
-  struct stat sb;
-  struct dirent *entry;
-  DIR *dirp;
 
-  /*if path too long*/
-  if(strlen(path) > PATH_MAX) {
-    printf("path too long\n");
-    return 0;
-  }
 
-  if( (lstat(path, &sb) != -1) && /*if path exists*/
-      /* and not a symlink*/
-      (!S_ISLNK(sb.st_mode)) &&             
-      /* and is a directory*/
-      ((dirp = opendir(path)) != NULL)
-   ) {
-        /*read entries until end of directory*/
-        while((entry = readdir(dirp)) != NULL) {
-          if(entry->d_type == DT_DIR) {
-            printf("subdirectory\n");
-            return 0;
-          }
-        }
-        return 1;
+      DIR *d;
+      struct dirent *entry;
+      struct stat sb;
+      char cwd[PATH_MAX];
+      
+      /*if path too long*/
+      if(strlen(path) > PATH_MAX) {
+         printf("path too long\n");
+         return 0;
       }
-  perror(path);
-  return 0;
+
+      /* if we can safely open the path */
+      if( ((d = opendir(path)) != NULL) ) {
+
+         /* read entries until end of directory */
+         while ((entry = readdir(d)) != NULL) {
+
+               /* debug prints */
+               /* if ( getcwd(cwd, sizeof(cwd)) == NULL ) {
+                  perror("getcwd");
+               }
+               printf("curr dir = %s\n", cwd);
+               */
+               /* printf("%s\n", entry->d_name); */
+
+               /* make sure entry is not the dot directories */
+               if ( !strcmp(entry->d_name, ".") || 
+                    !strcmp(entry->d_name, "..") 
+               ) {
+                  /* printf("ignore . or ..\n"); */
+                  continue; 
+               }
+
+               /* catch lstat error on a d_name???? */
+               if ( lstat( entry->d_name , &sb) == -1 ) {
+                  if (!strcmp(path, "..")) {
+                     perror(LSTAT_ERROR);
+                  }
+                  else {
+                     /* some other lstat error */
+                     perror(path);
+                  }
+                  exit(1);
+               }
+
+               /* if entry is a dir, not leaf */ 
+               if ( S_ISDIR( sb.st_mode ) ) {
+                  printf("not leaf\n");
+                  return 0;
+               }
+         }
+
+         /* must closedir because opendir */
+         closedir(d);
+         /* return true */
+         return 1;
+   }
+   printf("fail\n");
+   /* return false */
+   return 0;
 }
-
-
-
 
 int main(int argc, const char *argv[]) {
 
    if (argc < 2) {
-      perror("you suck");
+      printf("you suck, gimme 2 args!\n");
       exit(0);
    }
 
